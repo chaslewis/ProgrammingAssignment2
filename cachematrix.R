c5f0137a829109815d20be4e90572853a37232e6
chaslewis <foo@bar.net>
2014-12-21 19:12:23 -0500

    ready to go

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# The two functions in this source file illustrate a pattern for caching the 
+#   result of a potentially long-running computation.  In this case, the computation is 
+#   inverting a matrix.
+# The overall approach is to
+#   - Call a set-up function that creates a closure for the cached value and its key.
+#     It will expose methods to assign the key and save and retreive the cached value.
+#   - Call a function to perform the desired computation (matrix invere); it will use 
+#     the first function to retrieve the result, if cached, or comptue and save it, 
+#     before returning it.
+
+# illustration of use:
+# mx <- c(8, 1, 6, 3, 5, 7, 4, 9, 2)
+# dim(mx) = c(3,3)
+# cmx <- makeCacheMatrix(mx)
+# cacheSolve(cmx)         # returns inverse with cache miss message
+# cacheSolve(cmx)         # returns inverse with cache hit message
+
 
 ## create a "special matrix" to maintain a cached value: this is essentially
 #      an object with methods and encapsulated data as elements of a list
@@ -14,15 +30,15 @@
 #       setinv = function to cache the value of the inverse of the matrix
 #       getinv = function to return the cached value of the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mat = matrix()) {
     inv <- NULL
     # method sets the matrix value and resets the inverse to NULL (not calculated)
     set <-  function(y) {
-        x <<- y
-        i <<- NULL
+        mat <<- y
+        inv <<- NULL
     }
     # method to return the matrix
-    get <- function() x
+    get <- function() mat
     # method to cache the value of the inverse of x (calculated externally)
     setinv <- function(i) inv <<- i
     # method to get the cached inverse (may be NULL if invalid)
@@ -44,22 +60,25 @@ makeCacheMatrix <- function(x = matrix()) {
 #      set() method of the list-object
 #  
 
-cacheSolve <- function(x, ...) {
-    i <- x$getinv()
-    if (!is.null(i)) {
-        message("found cached inverse")   # will return i
+cacheSolve <- function(mkfn, ...) {
+    # get the inverse from cache, possibly null
+    inv <- mkfn$getinv()
+    if (!is.null(inv)) {
+        # result already cached, will return inv
+        message("found cached inverse")
     } else {
+        # result not cached, must compute
         message("inverse not cached--calculating")
-        data <- x$get()
+        mdta <- mkfn$get()
         # throw an error if matrix cannot be inverted
-        if (nrow(data) != ncol(data)) {
+        if (nrow(mdta) != ncol(mdta)) {
             stop("Attempt to invert a non-square matrix")   
-        } else if (det(data) == 0) {
+        } else if (det(mdta) == 0) {
             stop("Attempt to invert a singular matix")
         } else {
-            i <- solve(data, ...)          # will return i...
-            x$setinv(i)                    # and cache for future reference
+            inv <- solve(mdta, ...)          # will return inv...
+            mkfn$setinv(inv)                 # and cache for future reference
         }
     }    
-    i  
+    inv  
 }
